798d69144a3bb4af49385898afa07b84d82808e7
MUSTAFA ÇAKIR <ckrmustafa@yahoo.com>
2017-02-06 17:00:20 +0200

    Yeni uygulama eklendi

---
+++ Mustafa 2.Gun/3/esp_dht11_led_rgb_led_cloudmqtt/ESP8266_UserCongiguration.h
@@ -0,0 +1,15 @@
+/*
+ * ESP8266_UserCongiguration.h
+ *
+ *  Created on: 05 Feb 2017
+ *      Author: Orkan Murat CELIK
+ */
+
+#ifndef APPLICATION_ESP8266_USERCONFIGUTATION_H_
+#define APPLICATION_ESP8266_USERCONFIGUTATION_H_
+
+
+#define wifi_ssid "Electro"
+#define wifi_password "electro1234"
+
+#endif /* APPLICATION_ESP8266_USERCONFIGUTATION_H_ */
