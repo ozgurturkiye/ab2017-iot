2cd717a512294a35d806466472dbc7ead1d470c1
[ozgurturkiye] <[ozgurturkiye@gmail.com]>
2017-02-07 15:18:49 +0300

    veri tabanına kaydetme

--- mqtt_demo.py
+++ mqtt_demo.py
@@ -1,4 +1,6 @@
 import paho.mqtt.client as mqtt
+import sqlite3
+from datetime import datetime
 
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
@@ -6,18 +8,33 @@ def on_connect(client, userdata, flags, rc):
 
     # Subscribing in on_connect() means that if we lose the connection and
     # reconnect then subscriptions will be renewed.
-    client.subscribe("$SYS/#")
+    client.subscribe("/ab2017/mesaj")
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
     print(msg.topic+" "+str(msg.payload))
-    
+
     #Alt kısım gelen mesajı karakter dizisi olarak aldıktan sonra taklalar taklalar
     gelen_mesaj = str(msg.payload)
     print(type(gelen_mesaj))
     #gelen_mesajın 2 den balayıp son değer hariç alınması için
-    gelen_mesaj_parse = gelen_mesaj[2:-1] 
+    gelen_mesaj_parse = gelen_mesaj[2:-1]
     print(gelen_mesaj_parse)
+    konu = "/ab2017/mesaj"
+    an = datetime.now()
+    tarih = datetime.ctime(an)
+
+    with sqlite3.connect('vt.sqlite') as vt:
+        im = vt.cursor()
+
+        im.execute("""CREATE TABLE IF NOT EXISTS mqtt_depo
+            (tarih, konu, icerik)""")
+        veriler = (tarih, konu, gelen_mesaj_parse)
+
+        im.execute("""INSERT INTO mqtt_depo VALUES
+            (?, ?, ?)""", veriler)
+
+        vt.commit()
 
 client = mqtt.Client()
 client.on_connect = on_connect
@@ -29,4 +46,4 @@ client.connect("iot.eclipse.org", 1883, 60)
 # handles reconnecting.
 # Other loop*() functions are available that give a threaded interface and a
 # manual interface.
-client.loop_forever()mq
+client.loop_forever()
